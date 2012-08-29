85a885aa0365f419d90790cbdd372071a6773a01
Marcio Endo <marcio.endo@objectos.com.br>
2012-08-29 09:28:14 -0300

    objectos-rio is now a maven project
    - Adds keyboard X11 config files
    - RioCliModule
    - tabs...
    - atualiza versão de comuns-parent

---
+++ src/main/java/br/com/objectos/rio/command/HelpCommand.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2012 Objectos, Fábrica de Software LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package br.com.objectos.rio.command;
+
+import java.lang.annotation.Annotation;
+
+import br.com.objectos.comuns.cli.AbstractMainCommand;
+import br.com.objectos.rio.command.help.Help;
+
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+
+/**
+ * @author marcio.endo@objectos.com.br (Marcio Endo)
+ */
+class HelpCommand extends AbstractMainCommand {
+
+  static final String NAME = "help";
+
+  @Inject
+  public HelpCommand(Injector injector) {
+    super(injector);
+  }
+
+  @Override
+  public Class<? extends Annotation> getAnnotation() {
+    return Help.class;
+  }
+
+  @Override
+  public String getName() {
+    return NAME;
+  }
+
+}
