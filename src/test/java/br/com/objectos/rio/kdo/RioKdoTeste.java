723a19b7d5aa7e00a20bcb2f8c858cbb5b523c9e
Cristiane Iope Pelissari <cristiane.pelissari@objectos.com.br>
2013-10-31 16:22:34 -0200

    - Criação de classe de Teste RIO-20

---
+++ src/test/java/br/com/objectos/rio/kdo/RioKdoTeste.java
@@ -0,0 +1,25 @@
+/*
+ * RioKdoTeste.java criado em 31/10/2013
+ * 
+ * Propriedade de Objectos Fábrica de Software LTDA.
+ * Reprodução parcial ou total proibida.
+ */
+package br.com.objectos.rio.kdo;
+
+import org.testng.annotations.Guice;
+import org.testng.annotations.Test;
+
+import br.com.objectos.rio.RioTestModule;
+
+/**
+ * @author cristiane.pelissari@objectos.com.br (Cristiane Iope Pelissari)
+ */
+@Test
+@Guice(modules = { RioTestModule.class })
+public class RioKdoTeste {
+
+  public void parametros() {
+
+  }
+
+}
